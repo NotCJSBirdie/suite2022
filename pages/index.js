04f311b5f752384718903cf401532472ac2387be
Carl Serquina <carl.serquina@flexisourceit.com.au>
2022-05-29 22:00:45 +0800

    added mini variant drawer
    added material ui icons
    
    fixed lint errors

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,6 @@ import PeoplesIcon from "@rsuite/icons/Peoples";
 import TreeIcon from "@rsuite/icons/Tree";
 import GearIcon from "@rsuite/icons/Gear";
 import "rsuite/dist/rsuite.min.css";
-import styled from "styled-components";
 import { Container } from "rsuite";
 import { Sidebar } from "rsuite";
 import { Header } from "rsuite";
@@ -32,6 +31,7 @@ import ListItemIcon from "@mui/material/ListItemIcon";
 import ListItemText from "@mui/material/ListItemText";
 import InboxIcon from "@mui/icons-material/MoveToInbox";
 import MailIcon from "@mui/icons-material/Mail";
+import { useState } from "react";
 
 const drawerWidth = 240;
 
@@ -101,6 +101,17 @@ const Drawer = styled(MuiDrawer, {
 }));
 
 export default function Home() {
+  const theme = useTheme();
+  const [open, setOpen] = useState(false);
+
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
+
   return (
     <div>
       <Head>
@@ -109,53 +120,122 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <Container>
-        <Container>
-          <Sidebar
-            style={{
-              width: "20%",
-              display: "flex",
-              flexDirection: "column",
-            }}
-            collapsible
-          >
-            <Sidenav defaultOpenKeys={["3", "4"]}>
-              <Sidenav.Body>
-                <Nav activeKey="1">
-                  <Nav.Item eventKey="1" icon={<DashboardIcon />}>
-                    Dashboard
-                  </Nav.Item>
-                  <Nav.Item eventKey="2" icon={<PeoplesIcon />}>
-                    User Group
-                  </Nav.Item>
-                  <Nav.Menu eventKey="3" title="Advanced" icon={<TreeIcon />}>
-                    <Nav.Item eventKey="3-1">Geo</Nav.Item>
-                    <Nav.Item eventKey="3-2">Devices</Nav.Item>
-                    <Nav.Item eventKey="3-3">Loyalty</Nav.Item>
-                    <Nav.Item eventKey="3-4">Visit Depth</Nav.Item>
-                  </Nav.Menu>
-                  <Nav.Menu eventKey="4" title="Settings" icon={<GearIcon />}>
-                    <Nav.Item eventKey="4-1">Applications</Nav.Item>
-                    <Nav.Item eventKey="4-2">Channels</Nav.Item>
-                    <Nav.Item eventKey="4-3">Versions</Nav.Item>
-                    <Nav.Menu eventKey="4-5" title="Custom Action">
-                      <Nav.Item eventKey="4-5-1">Action Name</Nav.Item>
-                      <Nav.Item eventKey="4-5-2">Action Params</Nav.Item>
-                    </Nav.Menu>
-                  </Nav.Menu>
-                </Nav>
-              </Sidenav.Body>
-            </Sidenav>
-          </Sidebar>
-        </Container>
-
-        <Container>
-          <Header>
-            <h2>Page Title</h2>
-          </Header>
-          <Content>Content</Content>
-        </Container>
-      </Container>
+      <Box sx={{ display: "flex" }}>
+        <CssBaseline />
+        <AppBar position="fixed" open={open}>
+          <Toolbar>
+            <IconButton
+              color="inherit"
+              aria-label="open drawer"
+              onClick={handleDrawerOpen}
+              edge="start"
+              sx={{
+                marginRight: 5,
+                ...(open && { display: "none" }),
+              }}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" noWrap component="div">
+              Mini variant drawer
+            </Typography>
+          </Toolbar>
+        </AppBar>
+        <Drawer variant="permanent" open={open}>
+          <DrawerHeader>
+            <IconButton onClick={handleDrawerClose}>
+              {theme.direction === "rtl" ? (
+                <ChevronRightIcon />
+              ) : (
+                <ChevronLeftIcon />
+              )}
+            </IconButton>
+          </DrawerHeader>
+          <Divider />
+          <List>
+            {["Inbox", "Starred", "Send email", "Drafts"].map((text, index) => (
+              <ListItem key={text} disablePadding sx={{ display: "block" }}>
+                <ListItemButton
+                  sx={{
+                    minHeight: 48,
+                    justifyContent: open ? "initial" : "center",
+                    px: 2.5,
+                  }}
+                >
+                  <ListItemIcon
+                    sx={{
+                      minWidth: 0,
+                      mr: open ? 3 : "auto",
+                      justifyContent: "center",
+                    }}
+                  >
+                    {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                  </ListItemIcon>
+                  <ListItemText primary={text} sx={{ opacity: open ? 1 : 0 }} />
+                </ListItemButton>
+              </ListItem>
+            ))}
+          </List>
+          <Divider />
+          <List>
+            {["All mail", "Trash", "Spam"].map((text, index) => (
+              <ListItem key={text} disablePadding sx={{ display: "block" }}>
+                <ListItemButton
+                  sx={{
+                    minHeight: 48,
+                    justifyContent: open ? "initial" : "center",
+                    px: 2.5,
+                  }}
+                >
+                  <ListItemIcon
+                    sx={{
+                      minWidth: 0,
+                      mr: open ? 3 : "auto",
+                      justifyContent: "center",
+                    }}
+                  >
+                    {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                  </ListItemIcon>
+                  <ListItemText primary={text} sx={{ opacity: open ? 1 : 0 }} />
+                </ListItemButton>
+              </ListItem>
+            ))}
+          </List>
+        </Drawer>
+        <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
+          <DrawerHeader />
+          <Typography paragraph>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Rhoncus
+            dolor purus non enim praesent elementum facilisis leo vel. Risus at
+            ultrices mi tempus imperdiet. Semper risus in hendrerit gravida
+            rutrum quisque non tellus. Convallis convallis tellus id interdum
+            velit laoreet id donec ultrices. Odio morbi quis commodo odio aenean
+            sed adipiscing. Amet nisl suscipit adipiscing bibendum est ultricies
+            integer quis. Cursus euismod quis viverra nibh cras. Metus vulputate
+            eu scelerisque felis imperdiet proin fermentum leo. Mauris commodo
+            quis imperdiet massa tincidunt. Cras tincidunt lobortis feugiat
+            vivamus at augue. At augue eget arcu dictum varius duis at
+            consectetur lorem. Velit sed ullamcorper morbi tincidunt. Lorem
+            donec massa sapien faucibus et molestie ac.
+          </Typography>
+          <Typography paragraph>
+            Consequat mauris nunc congue nisi vitae suscipit. Fringilla est
+            ullamcorper eget nulla facilisi etiam dignissim diam. Pulvinar
+            elementum integer enim neque volutpat ac tincidunt. Ornare
+            suspendisse sed nisi lacus sed viverra tellus. Purus sit amet
+            volutpat consequat mauris. Elementum eu facilisis sed odio morbi.
+            Euismod lacinia at quis risus sed vulputate odio. Morbi tincidunt
+            ornare massa eget egestas purus viverra accumsan in. In hendrerit
+            gravida rutrum quisque non tellus orci ac. Pellentesque nec nam
+            aliquam sem et tortor. Habitant morbi tristique senectus et.
+            Adipiscing elit duis tristique sollicitudin nibh sit. Ornare aenean
+            euismod elementum nisi quis eleifend. Commodo viverra maecenas
+            accumsan lacus vel facilisis. Nulla posuere sollicitudin aliquam
+            ultrices sagittis orci a.
+          </Typography>
+        </Box>
+      </Box>
     </div>
   );
 }
